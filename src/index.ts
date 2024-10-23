286fd50498766e57a2f14f14361957fef72804fa
chrismbah <mbahchris46@gmail.com>
2024-10-23 17:17:23 +0100

    add new scripts

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,9 @@ import app from "./app";
 const PORT: number = 5050;
 
 app.listen(PORT, (): void => console.log(`running on port ${PORT}`));
+// "scripts": {
+//     "test": "jest --coverage",
+//     "start": "node dist/index.js", 
+//     "dev": "nodemon ./src/index.ts",
+//     "build": "tsc"
+//   },
