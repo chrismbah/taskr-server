c598c108ed3e067306602ee87060070856e9e32b
chrismbah <mbahchris46@gmail.com>
2024-10-24 10:46:48 +0100

    connect db

---
+++ config/db.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import "../config";
+
+// MongoDB Connection Function
+export const connectDB = async (): Promise<void> => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL as string);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error}`);
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
-import app from "./app";
+import express, { Application, Request, Response, NextFunction } from "express";
+import { router as userRoutes } from "./routes/user.routes";
+import { connectDB } from "../config/db";
+
+export const app: Application = express();
+
+app.use("/users", userRoutes);
+
+app.use("/", (req: Request, res: Response, next: NextFunction): void => {
+  res.json({ message: "Allo! Catch-all route." });
+});
 
 const PORT: number = 5050;
-
+connectDB();
 app.listen(PORT, (): void => console.log(`running on port ${PORT}`));
-// "scripts": {
-//     "test": "jest --coverage",
-//     "start": "node dist/index.js", 
-//     "dev": "nodemon ./src/index.ts",
-//     "build": "tsc"
-//   },
