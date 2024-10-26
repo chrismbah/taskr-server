35f0baac04d523241e1fb7ec3449159994f27067
chrismbah <mbahchris46@gmail.com>
2024-10-26 10:54:01 +0100

    new changes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Application } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import logger from "morgan"
+import logger from "morgan";
 import routes from "./routes";
 
 export const app: Application = express();
@@ -10,7 +10,8 @@ export const app: Application = express();
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use("/api", routes);
+
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import app from "./app";
-import { Request, Response, NextFunction } from "express";
+
 import { connectDB } from "./config/db";
 
 const PORT: number = 5050;
-app.use("/", (req: Request, res: Response, next: NextFunction): void => {
-  res.json({ message: "Allo! Catch-all route." });
-});
+
 connectDB();
 
 if (process.env.NODE_ENV !== "production" && process.env.NODE_ENV !== "test") {
